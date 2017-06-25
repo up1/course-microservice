2292769a0dd9185c01b322230997f62dfef29a18
somkiat <somkiat.p@gmail.com>
2017-06-25 11:29:02 +0700

    Fommating

--- demo/golang/demo_service/demo.go
+++ demo/golang/demo_service/demo.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-		"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/about/", about)
-		http.Handle("/prometheus/", promhttp.Handler())
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/about/", about)
+	http.Handle("/prometheus/", promhttp.Handler())
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to golang service, %!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Welcome to golang service, %!", r.URL.Path[1:])
 }
 
 type Message struct {
-    Text string
+	Text string
 }
 
-func about (w http.ResponseWriter, r *http.Request) {
+func about(w http.ResponseWriter, r *http.Request) {
 
-    m := Message{"Welcome to the SandovalEffect API, build v0.0.001.992, 6/22/2015 0340 UTC."}
-    b, err := json.Marshal(m)
+	m := Message{"Welcome to the SandovalEffect API, build v0.0.001.992, 6/22/2015 0340 UTC."}
+	b, err := json.Marshal(m)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-     w.Write(b)
+	w.Write(b)
 }
